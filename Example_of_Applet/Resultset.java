eb4c0069a85fce5e101bfea3ec53e23787d9fa49
Jaydeep Chauhan <66904825+Jaydeep2121@users.noreply.github.com>
2021-10-19 08:44:15 -0500

    Create Resultset.java

---
+++ Example_of_Applet/Resultset.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jaydeep Chauhan
+ */
+class Resultset {
+
+    boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int getInt(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getInt(String itm_no) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getString(String itm_name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
