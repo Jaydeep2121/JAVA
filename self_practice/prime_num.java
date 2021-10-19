f9bd95fa5211f84ed9c306545d77933f8bde4623
Jaydeep Chauhan <66904825+Jaydeep2121@users.noreply.github.com>
2021-10-19 08:52:21 -0500

    Create prime_num.java

---
+++ self_practice/prime_num.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import self_practice.Self_practice;
+/**
+ *
+ * @author Jaydeep Chauhan
+ */
+class prime_num {
+    public static void main(String[] args) {
+        Self_practice n=new Self_practice();
+        n.display();
+        n.get_prime();
+    }
+}
