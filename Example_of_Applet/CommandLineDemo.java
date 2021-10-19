e016c6aa0cc23c4a52fcb98e660f5ec66b4153d5
Jaydeep Chauhan <66904825+Jaydeep2121@users.noreply.github.com>
2021-10-19 08:40:19 -0500

    Create CommandLineDemo.java

---
+++ Example_of_Applet/CommandLineDemo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jaydeep Chauhan
+ */
+public class CommandLineDemo {
+    public static void main(String[] args)
+    {
+        int n,s;
+        n=Integer.parseInt(args[0]);
+        s = n*n;
+        System.out.println(s);
+    }
+}
